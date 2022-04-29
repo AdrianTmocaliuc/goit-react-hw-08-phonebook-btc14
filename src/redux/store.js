f63ce991e56cadf5dc0108b5099ccf731eb14847
ADRIAN TMOCALIUC <adriantmocaliuc@gmail.com>
2022-04-29 14:12:28 +0300

    createStore-to-cofiguerStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,11 @@
-import { createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { configureStore } from "@reduxjs/toolkit";
+// import { createStore, } from "redux";
+// import { composeWithDevTools ,  } from "redux-devtools-extension";
 import combineReducers from "./root-reducers";
 
-const store = createStore(combineReducers, composeWithDevTools());
+const store = configureStore({
+  reducer: combineReducers,
+  //   devTools: process.env.NODE_ENV !== "development",
+});
 
 export default store;
